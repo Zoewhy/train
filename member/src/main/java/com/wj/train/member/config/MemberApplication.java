dec4170e2b3293e47da701d745440a1b42e762c0
t_1503324918185_0787 <wj20000308@163.com>
2023-03-31 15:36:38 +0800

    新增member模块

---
+++ member/src/main/java/com/wj/train/member/config/MemberApplication.java
@@ -0,0 +1,15 @@
+package com.wj.train.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.wj")
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
