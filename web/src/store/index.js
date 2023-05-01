afd234d0d15b0532a9d21579affd5dd16c5f126e
Zoewhy <41788397+Zoewhy@users.noreply.github.com>
2023-05-01 22:17:11 +0800

    vuex配合H5 session缓存，解决浏览器刷新丢失数据的问题(引入自己封装的public\js\session-storage.js

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -5,14 +5,14 @@ const MEMBER = "MEMBER";
 
 export default createStore({
   state: {
-    member: JSON.parse(window.sessionStorage.getItem(MEMBER)) || {} //一种很常见写法避免空指针写法
+    member: window.SessionStorage.get(MEMBER) || {} //一种很常见写法避免空指针写法
   },
   getters: {
   },
   mutations: {
     setMember(state, _member){
         state.member = _member;
-        window.sessionStorage.setItem(MEMBER,JSON.stringify(_member));
+        window.SessionStorage.set(MEMBER, _member);
     }
   },
   actions: {
