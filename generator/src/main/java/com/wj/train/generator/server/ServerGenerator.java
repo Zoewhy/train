c1275d5ace2bc55298e4081ee4da96ab2f504f1a
Zoewhy <41788397+Zoewhy@users.noreply.github.com>
2023-06-03 22:49:19 +0800

    集成dom4j，得到当前持久层的xml文件, 得到表名和实体名

--- generator/src/main/java/com/wj/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/wj/train/generator/server/ServerGenerator.java
@@ -3,6 +3,7 @@ package com.wj.train.generator.server;
 import com.wj.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -23,13 +24,25 @@ public class ServerGenerator {
 //        Map<String, Object> param = new HashMap<>();
 //        param.put("domain", "Test");
 //        FreemarkerUtil.generator(toPath + "Test.java", param);
+        String generatorPath = getGenerator();
+
+
+        // 读取table节点
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+    }
+
+    private static String getGenerator() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         Map<String, String> map = new HashMap<>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
         saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
         Document document = saxReader.read(pomPath);
         Node node = document.selectSingleNode("//pom:configurationFile");
-        System.out.println(node.getText());
-
+        return node.getText();
     }
 }
