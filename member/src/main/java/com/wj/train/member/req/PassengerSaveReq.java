381a4908b7df799d6ecbdbb87e3be3f713ab738d
Zoewhy <41788397+Zoewhy@users.noreply.github.com>
2023-06-04 01:54:54 +0800

    生成Req实体类

--- member/src/main/java/com/wj/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/wj/train/member/req/PassengerSaveReq.java
@@ -1,25 +1,50 @@
 package com.wj.train.member.req;
 
+import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.util.Date;
-
 public class PassengerSaveReq {
+
+    /**
+     * id
+     */
     private Long id;
 
+    /**
+     * 会员id
+     */
     private Long memberId;
 
-    @NotBlank(message = "乘客姓名不能为空")
+    /**
+     * 姓名
+     */
+    @NotBlank(message = "【姓名】不能为空")
     private String name;
 
-    @NotBlank(message = "身份证信息不能为空")
+    /**
+     * 身份证
+     */
+    @NotBlank(message = "【身份证】不能为空")
     private String idCard;
-    @NotBlank(message = "乘客种类不能为空")
+
+    /**
+     * 旅客类型|枚举[PassengerTypeEnum]
+     */
+    @NotBlank(message = "【旅客类型】不能为空")
     private String type;
+
+    /**
+     * 新增时间
+     */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
+
+    /**
+     * 修改时间
+     */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
