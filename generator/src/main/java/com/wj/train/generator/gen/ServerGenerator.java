5f896bd551531b69679f5e41459eab9c587019c0
Zoewhy <41788397+Zoewhy@users.noreply.github.com>
2023-06-05 16:38:52 +0800

    重新生成火车座位页面，只读

--- generator/src/main/java/com/wj/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/wj/train/generator/gen/ServerGenerator.java
@@ -16,7 +16,7 @@ import java.util.*;
 
 public class ServerGenerator {
 
-    static boolean readOnly = false;
+    static boolean readOnly = true;
     static String vuePath = "admin/src/views/main/";
     static String servicePath = "[module]/src/main/java/com/wj/train/[module]/";
     static String pomPath =  "generator/pom.xml";
@@ -81,12 +81,12 @@ public class ServerGenerator {
         param.put("readOnly", readOnly);
         System.out.println("组装参数：" + param);
 
-        gen(Domain, param, "service","service");
-        //gen(Domain, param, "controller", "controller");
-        gen(Domain, param, "controller/admin", "adminController");
-        gen(Domain, param, "req", "saveReq");
-        gen(Domain, param, "req", "queryReq");
-        gen(Domain, param, "resp", "queryResp");
+//        gen(Domain, param, "service","service");
+//        //gen(Domain, param, "controller", "controller");
+//        gen(Domain, param, "controller/admin", "adminController");
+//        gen(Domain, param, "req", "saveReq");
+//        gen(Domain, param, "req", "queryReq");
+//        gen(Domain, param, "resp", "queryResp");
         genVue(do_main, param);
     }
 
