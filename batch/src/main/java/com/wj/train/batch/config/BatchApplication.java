6e3b0fb2e78bc1ce18ae29887d9b33fee1df5ad6
Zoewhy <41788397+Zoewhy@users.noreply.github.com>
2023-06-18 19:35:42 +0800

    增加batch定时调度模块

---
+++ batch/src/main/java/com/wj/train/batch/config/BatchApplication.java
@@ -0,0 +1,26 @@
+package com.wj.train.batch.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.wj")
+@MapperScan("com.wj.train.*.mapper")
+public class BatchApplication {
+
+    public static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BatchApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        LOG.info("启动成功！～");
+        LOG.info("测试地址:\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+    }
+
+
+}
